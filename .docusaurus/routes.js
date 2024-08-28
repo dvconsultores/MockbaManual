123d27cd59bb1892529d79c931f24a7c37e152b2
dvconsultores <adominguez@dvconsultores.com>
2024-08-28 16:10:15 -0400

    chore: Update menu options category and remove tutorial basics category

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '5ff'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
-    exact: true
-  },
-  {
     path: '/blog',
     component: ComponentCreator('/blog', 'b2f'),
     exact: true
@@ -114,37 +79,19 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '7cc'),
+    component: ComponentCreator('/docs', 'f2d'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', 'e25'),
+        component: ComponentCreator('/docs', 'd21'),
         routes: [
           {
             path: '/docs',
-            component: ComponentCreator('/docs', '44e'),
+            component: ComponentCreator('/docs', 'c93'),
             routes: [
               {
                 path: '/docs/category/menu---options',
                 component: ComponentCreator('/docs/category/menu---options', '8af'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/docs/category/tutorial---extras',
-                component: ComponentCreator('/docs/category/tutorial---extras', '9ad'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/docs/install-app/manage-docs-versions',
-                component: ComponentCreator('/docs/install-app/manage-docs-versions', 'e18'),
-                exact: true,
-                sidebar: "tutorialSidebar"
-              },
-              {
-                path: '/docs/install-app/translate-your-site',
-                component: ComponentCreator('/docs/install-app/translate-your-site', '376'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
